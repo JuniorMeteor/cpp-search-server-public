120a1af306470272f9484e632ac008b1ce7eb36b
JuniorMeteor <96436209+JuniorMeteor@users.noreply.github.com>
2022-11-02 12:02:19 +0300

    some code had been written
    it should work, but need to test anyway

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -1,6 +1,42 @@
 #include "search_server.h"
 #include "remove_duplicates.h"
 
+#include <string>
+#include <map>
+#include <set>
+#include <iostream>
+
 void RemoveDuplicates(SearchServer& search_server) {
-    return; // привет костыль **************************************
+    // set of words which corresponds to some document
+    std::map<std::set<std::string>, int> words_in_docs;
+    // list of duplicates
+    vector<int> duplicates_to_remove;
+    // optimization
+    duplicates_to_remove.reserve(search_server.GetDocumentCount());
+    // checking each doc
+    for (auto iter_id = search_server.begin(); iter_id != search_server.end(); ++iter_id) {
+        std::set<std::string> words;
+        // get words from the doc and put them to <words>
+        for (auto& [word, _] : search_server.GetWordFrequencies(*iter_id)) {
+            words.insert(word);
+        }
+        
+        if (words_in_docs.count(words)) {
+            // if current set of word already exists, doc id is added to the list of duplicates
+            // and it is not added to <words_in_docs>
+            duplicates_to_remove.push_back(*iter_id);
+        } else {
+            // if not, add new <words> and first <id> which contains <words>
+            words_in_docs.insert({words, *iter_id});
+        }
+        
+    }
+    for (auto& duplicate : duplicates_to_remove) {
+        // removing duplicates
+        std::cout << "Found duplicate document id " << duplicate << std::endl;
+        search_server.RemoveDocument(duplicate);
+    }
 }
+
+//std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings)
+//const std::map<std::string, double>& SearchServer::GetWordFrequencies(int document_id) const
