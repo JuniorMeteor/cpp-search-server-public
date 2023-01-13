255a750852bc3f5fee1227d4b4caca39c5d06f49
Anton Pavlyuk <anton06071997@mail.ru>
2023-01-13 20:09:27 +0300

    benchmark was updated

--- benchmark_last_lesson.h
+++ benchmark_last_lesson.h
@@ -1,11 +1,10 @@
-#pragma once
+#include "search_server.h"
+#include "log_duration.h"
+#include <execution>
 #include <iostream>
 #include <random>
 #include <string>
 #include <vector>
-#include "search_server.h"
-#include "log_duration.h"
-#include "process_queries.h"
 
 using namespace std;
 
@@ -18,30 +17,28 @@ string GenerateWord(mt19937& generator, int max_length) {
     }
     return word;
 }
-
 vector<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
     vector<string> words;
     words.reserve(word_count);
     for (int i = 0; i < word_count; ++i) {
         words.push_back(GenerateWord(generator, max_length));
     }
-    sort(words.begin(), words.end());
     words.erase(unique(words.begin(), words.end()), words.end());
     return words;
 }
-
-string GenerateQuery(mt19937& generator, const vector<string>& dictionary, int max_word_count) {
-    const int word_count = uniform_int_distribution(1, max_word_count)(generator);
+string GenerateQuery(mt19937& generator, const vector<string>& dictionary, int word_count, double minus_prob = 0) {
     string query;
     for (int i = 0; i < word_count; ++i) {
         if (!query.empty()) {
             query.push_back(' ');
         }
+        if (uniform_real_distribution<>(0, 1)(generator) < minus_prob) {
+            query.push_back('-');
+        }
         query += dictionary[uniform_int_distribution<int>(0, dictionary.size() - 1)(generator)];
     }
     return query;
 }
-
 vector<string> GenerateQueries(mt19937& generator, const vector<string>& dictionary, int query_count, int max_word_count) {
     vector<string> queries;
     queries.reserve(query_count);
@@ -50,25 +47,29 @@ vector<string> GenerateQueries(mt19937& generator, const vector<string>& diction
     }
     return queries;
 }
-
-template <typename QueriesProcessor>
-void Test(string_view mark, QueriesProcessor processor, const SearchServer& search_server, const vector<string>& queries) {
+template <typename ExecutionPolicy>
+void Test(string_view mark, const SearchServer& search_server, const vector<string>& queries, ExecutionPolicy&& policy) {
     LOG_DURATION(mark);
-    const auto documents_lists = processor(search_server, queries);
+    double total_relevance = 0;
+    for (const string_view query : queries) {
+        for (const auto& document : search_server.FindTopDocuments(policy, query)) {
+            total_relevance += document.relevance;
+        }
+    }
+    cout << total_relevance << endl;
 }
-
-#define TEST(processor) Test(#processor, processor, search_server, queries)
-
-/*
+#define TEST(policy) Test(#policy, search_server, queries, execution::policy)
 int main() {
     mt19937 generator;
-    const auto dictionary = GenerateDictionary(generator, 2'000, 25);
-    const auto documents = GenerateQueries(generator, dictionary, 20'000, 10);
+    const auto dictionary = GenerateDictionary(generator, 1000, 10);
+    const auto documents = GenerateQueries(generator, dictionary, 10'000, 70);
     SearchServer search_server(dictionary[0]);
     for (size_t i = 0; i < documents.size(); ++i) {
         search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, { 1, 2, 3 });
     }
-    const auto queries = GenerateQueries(generator, dictionary, 2'000, 7);
-    TEST(ProcessQueries);
+    const auto queries = GenerateQueries(generator, dictionary, 100, 70);
+    TEST(seq);
+    TEST(par);
+
+    return 0;
 }
-*/
